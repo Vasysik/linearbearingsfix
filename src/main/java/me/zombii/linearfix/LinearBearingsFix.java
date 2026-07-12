a86f6038382db72a7dadb82c047a7787aa41f9fe
Mr-Zombii <thezombiigames@gmail.com>
2026-07-12 16:26:48 -0500

    inc ver

--- src/main/java/me/zombii/linearfix/LinearBearingsFix.java
+++ src/main/java/me/zombii/linearfix/LinearBearingsFix.java
@@ -1,6 +1,5 @@
 package me.zombii.linearfix;
 
-import com.bearing.linearbearing.LinearBearing;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -11,7 +10,6 @@ import net.neoforged.fml.ModContainer;
 
 @Mod(LinearBearingsFix.MODID)
 public class LinearBearingsFix {
-    LinearBearing
     public static final String MODID = "linearbearingsfix";
     public static final Logger LOGGER = LogUtils.getLogger();
 
